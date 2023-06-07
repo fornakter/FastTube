18adab44c45b9df0a2bc7fb26b47e973f23a131b
AdamF <fornakter@gmail.com>
2023-06-07 12:15:47 +0200

    Enter gotowy

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-function textEnter(){
-    let textarea = document.getElementById("serchBar")
-    textarea.addEventListener("keydown", (e) => {
-        if (e == 13) {
-        //if (!e.repeat) {
-          alert(`Key "${e.key}" pressed [event: keydown]`);
-        } else {
-          alert(`Key "${e.key}" repeating [event: keydown]`);
-        }
-      });
-};
-
 function goToYoutube() {
     let searchText = document.getElementById("serchBar").value;
     let urlYouTube = "https://www.youtube.com/results?search_query=" + searchText
