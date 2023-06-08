c8b523f154029353201d803319abbb6ee048cd87
AdamF <fornakter@gmail.com>
2023-06-08 21:03:08 +0200

    Błędy poprawiono

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function goToYoutube() {
-    let searchText = document.getElementById("serchBar").value;
+    let searchText = document.getElementById("text").value;
     let urlYouTube = "https://www.youtube.com/results?search_query=" + searchText
     window.open(urlYouTube, "_self")
+};
+
+function youtubeButton(){
+    let ytButton = "http://www.youtube.com"
+    window.open(ytButton, "_self")
+};
+
+function aboutButton(){
+    let ytButton = "http://fasttube.ddns.net"
+    window.open(ytButton, "_self")
+};
+
+function studioButton(){
+    let ytButton = "https://studio.youtube.com/"
+    window.open(ytButton, "_self")
 };
